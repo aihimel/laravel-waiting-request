c143245b9ea2178970411015cce6feff83508a66
Aftabul Islam <toaihimel@gmail.com>
2026-05-12 10:07:51 +0600

    docs: doc block added

--- src/LWRequestService.php
+++ src/LWRequestService.php
@@ -5,26 +5,55 @@ namespace Aihimel\LaravelWaitingRequest;
 use Illuminate\Support\Facades\Cache;
 
 class LWRequestService {
+	/**
+	 * Generating a unique key to store the blocker in cache
+	 *
+	 * @param string $classPath
+	 * @param int    $resourceId
+	 *
+	 * @return string
+	 */
 	protected function generateKey( string $classPath, int $resourceId ): string
 	{
 		return $classPath . "_" . $resourceId;
 	}
 
-	public function addBlocker(
-        string $classPath,
-        int $resourceId,
-	): bool {
+	/**
+	 * Create a blocker for a resource
+	 *
+	 * @param string $classPath
+	 * @param int    $resourceId
+	 *
+	 * @return bool
+	 */
+	public function addBlocker( string $classPath, int $resourceId ): bool {
 		return Cache::add(
 			$this->generateKey( $classPath, $resourceId ),
 			true,
 		);
 	}
 
+	/**
+	 * Resolve or remove a blocker
+	 *
+	 * @param string $classPath
+	 * @param int    $resourceId
+	 *
+	 * @return bool
+	 */
 	public function resolveBlocker( string $classPath, int $resourceId ): bool
 	{
 		return Cache::forget( $this->generateKey( $classPath, $resourceId ) );
 	}
 
+	/**
+	 * Check if a resource is blocked
+	 *
+	 * @param string $classPath
+	 * @param int    $resourceId
+	 *
+	 * @return bool
+	 */
 	public function isBlocked( string $classPath, int $resourceId ): bool
 	{
 		return Cache::has( $this->generateKey( $classPath, $resourceId ) );
