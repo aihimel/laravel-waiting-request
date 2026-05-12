6157fe41c34961fc16732deb3a780bf109b79523
Aftabul Islam <toaihimel@gmail.com>
2026-05-12 14:31:28 +0600

    test: configuration test added for cascading and publishing

---
+++ tests/Feature/ConfigurationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Aihimel\LaravelWaitingRequest\Tests\Feature;
+
+use Aihimel\LaravelWaitingRequest\Tests\TestCase;
+use Illuminate\Support\Facades\File;
+
+class ConfigurationTest extends TestCase
+{
+    public function testConfigurationCascading()
+    {
+        // Check default value
+        $this->assertEquals('lw_request_', config('waiting-request.cache_prefix'));
+
+        // Override value
+        config(['waiting-request.cache_prefix' => 'custom_prefix_']);
+
+        // Check overridden value
+        $this->assertEquals('custom_prefix_', config('waiting-request.cache_prefix'));
+    }
+
+    public function testConfigurationCanBePublished()
+    {
+        $configPath = config_path('waiting-request.php');
+
+        // Ensure the file does not exist before publishing
+        if (File::exists($configPath)) {
+            File::delete($configPath);
+        }
+
+        $this->artisan('vendor:publish', [
+            '--tag' => 'waiting-request-config',
+        ])->assertExitCode(0);
+
+        $this->assertTrue(File::exists($configPath));
+
+        // Cleanup
+        File::delete($configPath);
+    }
+}
