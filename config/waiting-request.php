715c371e53bfe21d23ba0f995947a9677a42d4fd
Aftabul Islam <toaihimel@gmail.com>
2026-05-13 08:49:55 +0600

    feat: waiting request for blocker resolving

--- config/waiting-request.php
+++ config/waiting-request.php
@@ -12,4 +12,26 @@ return [
     |
     */
     'cache_prefix' => env('LW_REQUEST_CACHE_PREFIX', 'lw_request_'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Timeout
+    |--------------------------------------------------------------------------
+    |
+    | The maximum time in seconds to wait for a blocker to be resolved
+    | when using the whenResolved method.
+    |
+    */
+    'timeout' => 5, // seconds
+
+    /*
+    |--------------------------------------------------------------------------
+    | Check Interval
+    |--------------------------------------------------------------------------
+    |
+    | The time in milliseconds to wait between each check for the blocker
+    | to be resolved.
+    |
+    */
+    'check_interval' => 250, // milliseconds
 ];
